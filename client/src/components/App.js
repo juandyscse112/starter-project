1c19d1d3d2619879338a5898a4b01982f4acf8eb
chumothyLee <llol5394@gmail.com>
2018-05-09 14:16:55 -0700

    begin pwa support

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,64 +1,49 @@
-/**
- * UI for testing functionality of PetersComponent 
- * @author chumothyLee
- */
 import React, { Component } from 'react';
-import { Container, Row, Col, Form, Label, Input } from 'reactstrap';
-import { GithubPicker } from 'react-color';
-import PetersComponent from './PetersComponent';
+import { Router, browserHistory, Route, Link } from 'react-router';
+import logo from '../logo.svg';
+import './App.css';
+
+const Page = ({ title }) => (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>{title}</h2>
+      </div>
+      <p className="App-intro">
+        This is the {title} page.
+      </p>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to="/about">About</Link>
+      </p>
+      <p>
+        <Link to="/settings">Settings</Link>
+      </p>
+    </div>
+);
+
+const Home = (props) => (
+  <Page title="Home"/>
+);
+
+const About = (props) => (
+  <Page title="About"/>
+);
+
+const Settings = (props) => (
+  <Page title="Settings"/>
+);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        color : '#000',
-        times : 1
-    }
-  }
-
-  /**
-   * Updates component's state.color value when user selects a new color on GithubPicker
-   * @public
-   * @param {color} color New color value for PetersComponent. Use hex key.
-   * @returns {null}
-   */
-  handleChange = (color) => {
-    console.log(color);
-    this.setState({ color : color.hex });
-  }
-
-  /**
-   * Updates component's state.time value when user inputs numerical value to #timeSelector
-   * @public
-   * @param {*} event Event from which to take update number value
-   * @returns {null}
-   */
-  handleTimeChange = (event) => {
-    const times = event.target.value;
-    console.log(times);
-    if (!isNaN(times) && times > 0) {
-      this.setState({ times : event.target.value });
-    }
-  }
-
   render() {
     return (
-        <Container>
-            <Row>
-                <Col>
-                  <Form>
-                    <Label for="colorPicker"> Color of text </Label>
-                    <GithubPicker id="colorPicker" color={this.state.color} onChange={this.handleChange}/>
-                    <Label for="timesSelector"> Times to print text </Label>
-                    <Input id="timesSelector" type="textarea" onChange={this.handleTimeChange}/>
-                  </Form>
-                </Col>
-                <Col>
-                    <PetersComponent color={this.state.color} times={this.state.times}/>
-                </Col>
-            </Row>
-        </Container>
+      <Router history={browserHistory}>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/settings" component={Settings}/>
+      </Router>
     );
   }
 }
--- client/src/components/PetersComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PetersComponent from './PetersComponent';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PetersComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
